45a540af66e19fece53249ec3acae6fad7464cf1
Kegnarok <kegnarok.rm@gmail.com>
2017-04-28 10:23:44 +0200

    error {}

--- src/error.h
+++ src/error.h
@@ -8,15 +8,16 @@
  *  @name throw - Prints the string to the screen, along with the location of
  *  the error, and loop forever.
  */
-#define throw(string) \
-  write_string("\nFile "); \
-  write_string(__FILE__); \
-  write_string(", line "); \
-  write_int(__LINE__); \
-  write_string(": ERROR "); \
-  write_string(string); \
-  \
-  for(;;) {\
-    __asm__ __volatile__("hlt");} // idle state, still reacts to interrupts
+#define throw(string) {                                                 \
+  write_string("\nFile ");                                              \
+  write_string(__FILE__);                                               \
+  write_string(", line ");                                              \
+  write_int(__LINE__);                                                  \
+  write_string(": ERROR ");                                             \
+  write_string(string);                                                 \
+                                                                        \
+  for(;;) {                                                             \
+    __asm__ __volatile__("hlt");}                                       \
+}
 
 #endif
