f146f14881b8fa9314c068a8678260fbc1bc1126
Kegnarok <kegnarok.rm@gmail.com>
2017-05-12 00:09:45 +0200

    Corrected ELF parsing

--- src/elf.c
+++ src/elf.c
@@ -25,28 +25,42 @@ void mem_copy_page(void *dest, const void *src, u_int32 length, u_int32 virtuals
 
 u_int32 check_and_load(void *elf_file, u_int32 virtuals[])
 {
+  kloug(100, "Checking and loading ELF\n");
+
   elf_header_t *elf_header = (elf_header_t *)elf_file;
 
-  u_int8 *magic_number = (u_int8 *)elf_header->magic_number;
+  u_int8 *magic_number = (u_int8 *)&elf_header->magic_number;
 
   /* Checks */
   if (!(magic_number[0] == 0x7F &&
         magic_number[1] == 'E' &&
         magic_number[2] == 'L' &&
-        magic_number[3] == 'F' &&
-        elf_header->nb_bits == 1 &&
-        elf_header->endian == 1 &&
-        elf_header->elf_type == 2 &&
-        elf_header->instruction_set == 3 &&
-        elf_header->entry_point < START_OF_USER_CODE)) {
-    kloug(100, "%x %d %d %d %d %x\n", elf_header->magic_number, elf_header->nb_bits, elf_header->endian, \
-          elf_header->elf_type, elf_header->instruction_set, elf_header->entry_point);
-    throw("Invalid elf header");
+        magic_number[3] == 'F')) {
+    throw("Not a ELF file");
   }
+  if (!(elf_header->nb_bits == 1)) {
+    throw("Not a 32 bits executable");
+  }
+  if (!(elf_header->endian == 1)) {
+    throw("Not a little endian executable");
+  }
+  if (!(elf_header->elf_type == 2)) {
+    throw("Not an executable");
+  }
+  if (!(elf_header->instruction_set == 3)) {
+    throw("Not a x86 executable");
+  }
+  if (!(elf_header->entry_point >= START_OF_USER_CODE)) {
+    writef("Wrong entry point: %x compared to %x\n", elf_header->entry_point, START_OF_USER_CODE);
+    throw("Wrong entry point");
+  }
+
+  kloug(100, "Valid header\n");
 
   for (u_int16 segment_number = 0; segment_number < elf_header->pht_entry_nb; segment_number += 1) {
     program_header_entry_t *segment = (program_header_entry_t *) \
       (elf_file + elf_header->program_header_table + segment_number * elf_header->pht_entry_size);
+    writef("Segment %d\n", segment_number);
 
     if (segment->segment_type == Load) {
       void *address = (void *)segment->segment_virtual_address;
