9f80d2c4b12899bf88aacd6ee9783bda218dd180
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2017-05-12 16:40:10 +0200

    So you think you can bitset?

--- src/bitset.h
+++ src/bitset.h
@@ -16,9 +16,31 @@ typedef struct bitset {
 
 bitset_t empty_bitset(u_int32 lenght);
 
+/**
+ *  @name set_bit - Sets or clears one bit of the bitset
+ *  @param b      - The bitset
+ *  @param bit    - The bit to set or clear
+ *  @param value  - The new value
+ */
 void set_bit(bitset_t b, u_int32 bit, bool value);
+
+/**
+ *  @name get_bit - Gets one bit from the bitset
+ *  @param b      - The bitset
+ *  @param bit    - The bit to get
+ */
 bool get_bit(bitset_t b, u_int32 bit);
 
+/**
+ *  @name first_false_bit - Gets the position of the first clear bit
+ *  @param b              - The bitset
+ *  @return               - The position, or -1 [2^32] if none was found
+ */
 u_int32 first_false_bit(bitset_t b);
 
+/**
+ *  @name param_ffb - Same as first_false_bit, but with a starting bit
+ */
+u_int32 param_ffb(bitset_t b, u_int32 first);
+  
 #endif
