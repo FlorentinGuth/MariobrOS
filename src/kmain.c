4dc14252b1ec297e04ab21565d7fc6f3d3231cf8
Kegnarok <kegnarok.rm@gmail.com>
2017-05-13 22:07:01 +0200

    mairj...

--- src/kmain.c
+++ src/kmain.c
@@ -51,8 +51,6 @@ int kmain(multiboot_info_t* mbd, u_int32 stack_start, u_int32 stack_size)
   /* Last but not least, the shell */
   shell_install();
 
-  scheduler_install();
-
   /* Enables interruptions */
   __asm__ __volatile__ ("sti");
   for(;;)
