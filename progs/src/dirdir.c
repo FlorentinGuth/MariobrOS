af7ae64c92697ebfaf4fd03c65df48674f3e592e
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2017-05-18 14:57:28 +0200

    (fix)

--- progs/src/dirdir.c
+++ progs/src/dirdir.c
@@ -5,7 +5,10 @@ int main()
   string name = "t0";
   u_int32 inode = 2;
   for(int i = 0; i < 1000; i++) {
-    mkdir((void*) name, inode);
+    if(mkdir((void*) name, inode)) {
+      writef("Failed at no %u\n", i);
+      return 1;
+    }
     inode = find_dir((void*) name, inode);
     name[1] = 49 + (i % 10);
   }
